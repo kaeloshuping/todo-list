9354b736c8179323d0c0e27c681315cac2d1c33e
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2024-02-07 20:34:47 +0200

    created file for gui and added sample code to create page header

---
+++ src/gui.js
@@ -0,0 +1,9 @@
+function userInterface() {
+    const pageHeader = document.createElement("div");
+    pageHeader.id = "page-header"
+    return { pageHeader };
+};
+
+export { userInterface }
+
+// create page header
