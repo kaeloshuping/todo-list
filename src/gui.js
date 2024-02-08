50186a6093ca9ce73f783c0e3290d896fedacd5c
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2024-02-08 21:33:48 +0200

    added elements for side bar content

--- src/gui.js
+++ src/gui.js
@@ -3,12 +3,47 @@ const userInterface = function() {
         const headerContainer = document.createElement("div");
         headerContainer.id = "page-header";
 
+        const headerText = document.createElement("div");
+        headerText.id = "header-text";
+        headerText.innerHTML = "To Do List"
+
+        headerContainer.appendChild(headerText);
+
         return headerContainer;
     };
 
     const sideBar = function() {    
         const sideBarContainer = document.createElement("div");
         sideBarContainer.id = "side-bar"; 
+
+        const taskSelectors = document.createElement("ul");
+        taskSelectors.id = "task-selector";
+
+        const home = document.createElement("li");
+        home.id = "all-tasks";
+        home.innerHTML = "Home";
+
+        const todayTasks = document.createElement("li");
+        todayTasks.id = "today-tasks";
+        todayTasks.innerHTML = "Todays Tasks";
+
+        const upcomingTasks = document.createElement("li");
+        upcomingTasks.id = "upcoming-tasks";
+        upcomingTasks.innerHTML = "Upcoming Tasks";
+
+        const completedTasks = document.createElement("li");
+        completedTasks.id = "completed-tasks";
+        completedTasks.innerHTML = "Completed Tasks";
+
+        taskSelectors.appendChild(home);
+        taskSelectors.appendChild(todayTasks);
+        taskSelectors.appendChild(upcomingTasks);
+        taskSelectors.appendChild(completedTasks);
+
+        const hr = document.createElement("hr");
+
+        sideBarContainer.appendChild(taskSelectors);
+        sideBarContainer.appendChild(hr);
         
         return sideBarContainer;
     };
