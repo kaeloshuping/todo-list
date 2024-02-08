438c4c03bd5acf59dc64aafb2c49500300800d35
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2024-02-08 20:44:28 +0200

    created factory function to handle gui

--- src/gui.js
+++ src/gui.js
@@ -1,9 +1,32 @@
-function userInterface() {
-    const pageHeader = document.createElement("div");
-    pageHeader.id = "page-header"
-    return { pageHeader };
+const userInterface = function() {
+    const pageHeader = function() {
+        const headerContainer = document.createElement("div");
+        headerContainer.id = "page-header";
+
+        return headerContainer;
+    };
+
+    const sideBar = function() {    
+        const sideBarContainer = document.createElement("div");
+        sideBarContainer.id = "side-bar"; 
+        
+        return sideBarContainer;
+    };
+
+    const pageFooter = function() {
+        const footerContainer = document.createElement("div");
+        footerContainer.id = "page-footer";
+
+        const footerText = document.createElement("div");
+        footerText.id = "footer-text";
+        footerText.innerHTML = "Developed by @ Copasetic Demon"
+
+        footerContainer.appendChild(footerText);
+
+        return footerContainer;
+    };
+
+    return { pageHeader, sideBar, pageFooter };
 };
 
-export { userInterface }
-
-// create page header
+export { userInterface };
