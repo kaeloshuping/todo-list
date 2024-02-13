2736777f479c9470d384ac541b007b87af10be8c
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2024-02-13 20:01:53 +0200

    changed handling of page gui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import { userInterface } from "./gui";
 
-const mainContainer = document.getElementById("main-container");
-
-let gui = userInterface();
-
-mainContainer.appendChild(gui.pageHeader());
-mainContainer.appendChild(gui.sideBar());
-mainContainer.appendChild(gui.pageFooter());
-
-// console.log(userText.pageHeader);
+userInterface();
