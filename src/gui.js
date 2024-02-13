e2d4ddb1cd17abed6844c9363b13a4b864c0f215
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2024-02-13 20:00:29 +0200

    moved header, footer and side bar to different modules

--- src/gui.js
+++ src/gui.js
@@ -1,67 +1,18 @@
-const userInterface = function() {
-    const pageHeader = function() {
-        const headerContainer = document.createElement("div");
-        headerContainer.id = "page-header";
+import { headerFooter } from "./header-footer";
+import { sideBar } from "./side-bar";
 
-        const headerText = document.createElement("div");
-        headerText.id = "header-text";
-        headerText.innerHTML = "To Do List"
+function userInterface() {
+    const mainContainer = document.getElementById("main-container");
 
-        headerContainer.appendChild(headerText);
+    const pageHeaderFooter = headerFooter();
 
-        return headerContainer;
-    };
+    const pageSideBar = sideBar();
 
-    const sideBar = function() {    
-        const sideBarContainer = document.createElement("div");
-        sideBarContainer.id = "side-bar"; 
+    mainContainer.appendChild(pageHeaderFooter.pageHeader());
+    mainContainer.appendChild(pageSideBar.sideBarContainer);
+    mainContainer.appendChild(pageHeaderFooter.pageFooter());
 
-        const taskSelectors = document.createElement("ul");
-        taskSelectors.id = "task-selector";
-
-        const home = document.createElement("li");
-        home.id = "all-tasks";
-        home.innerHTML = "Home";
-
-        const todayTasks = document.createElement("li");
-        todayTasks.id = "today-tasks";
-        todayTasks.innerHTML = "Todays Tasks";
-
-        const upcomingTasks = document.createElement("li");
-        upcomingTasks.id = "upcoming-tasks";
-        upcomingTasks.innerHTML = "Upcoming Tasks";
-
-        const completedTasks = document.createElement("li");
-        completedTasks.id = "completed-tasks";
-        completedTasks.innerHTML = "Completed Tasks";
-
-        taskSelectors.appendChild(home);
-        taskSelectors.appendChild(todayTasks);
-        taskSelectors.appendChild(upcomingTasks);
-        taskSelectors.appendChild(completedTasks);
-
-        const hr = document.createElement("hr");
-
-        sideBarContainer.appendChild(taskSelectors);
-        sideBarContainer.appendChild(hr);
-        
-        return sideBarContainer;
-    };
-
-    const pageFooter = function() {
-        const footerContainer = document.createElement("div");
-        footerContainer.id = "page-footer";
-
-        const footerText = document.createElement("div");
-        footerText.id = "footer-text";
-        footerText.innerHTML = "Developed by @ Copasetic Demon"
-
-        footerContainer.appendChild(footerText);
-
-        return footerContainer;
-    };
-
-    return { pageHeader, sideBar, pageFooter };
+    return mainContainer;
 };
 
 export { userInterface };
